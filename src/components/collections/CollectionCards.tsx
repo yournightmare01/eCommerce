89c2df11ff818ff35e8c9d5a1e83bfed8419907e
yournightmare01 <zlatkor1001@gmail.com>
2021-11-02 19:00:58 +0100

    working on filters

--- src/components/collections/CollectionCards.tsx
+++ src/components/collections/CollectionCards.tsx
@@ -1,16 +1,41 @@
 import { CollectionData } from './CollectionItems';
 import classes from './CollectionCards.module.scss';
 import sneakers from '../../images/image-product-1.jpg';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
+// import Select from 'react-dropdown-select';
+
+const options = [
+  {
+    option: 'Man',
+  },
+  { option: 'Women' },
+];
 
 const CollectionCards = () => {
+  const [selected, setIsSelected] = useState(false);
+
+  const genderChangeHandler = () => {
+    setIsSelected(!selected);
+  };
   return (
     <Fragment>
+      <div className={classes.genderContainer}>
+        {options.map((item, i) => {
+          return (
+            <div className={classes.genderSelect}>
+              <input
+                type='checkbox'
+                value={item.option}
+                name={item.option}
+                id={item.option}
+              />
+              <label htmlFor={item.option}>{item.option}</label>
+            </div>
+          );
+        })}
+      </div>
       <div className={classes.filterNav}>
         <div className={classes.filters}>
-          <div>
-            <h4>Filter1</h4>
-          </div>
           <div>
             <h4> Filter2</h4>
           </div>
@@ -22,8 +47,10 @@ const CollectionCards = () => {
           </div>
         </div>
 
-        <div className={classes.sort}>
-          <h4>SORT</h4>
+        <div className={classes.sortContainer}>
+          <div className={classes.sort}>
+            <h4>SORT</h4>
+          </div>
         </div>
       </div>
       <div className={classes.layout}>
